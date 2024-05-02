2bccf8dab8472c99caec75bba9eb0628447917fa
kristyyip <kristyyip51998@gmail.com>
2024-05-02 14:47:57 -0400

    included necessary packages in index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const {readFile, writeFile} = require("fs").promises;
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [];
