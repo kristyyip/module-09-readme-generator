c9d8a6bc156da21edbe0ed473ee9f33575bca1a9
kristyyip <kristyyip51998@gmail.com>
2024-05-02 15:27:20 -0400

    created array of questions

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const {readFile, writeFile} = require("fs").promises;
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your GitHub username?", 
+    "What is your email address?", 
+    "What is your project's name?",
+    "Please write a short description of your project:",
+    "What kind of license should your project have?",
+    "What command should be run to install dependencies?",
+    "What command should be run to run tests?",
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
